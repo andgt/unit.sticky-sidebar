a7ec87e7e327f41ce85a46fc74be098fb215e71c
Anatoly <anatolijdegtyar@yandex.ru>
2021-02-20 13:22:15 +0300

    Исправляет script.js

--- js/script.js
+++ js/script.js
@@ -2,20 +2,9 @@
 
 let menuItem = document.querySelectorAll(".nav__menu-item");
 
-/*for (var i = menuItem.length - 1; i <= 9; i++) {
-	menuItem[i].classList.toggle("nav__menu-item-open");
-}*/
-
-
-/*menuItem.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	console.log(menuItem);
-	menuItem.classList.toggle("nav__menu-item-open");
-});*/
-
 for (let menu of menuItem) {
 	menu.addEventListener("click", function (evt) {
-	evt.preventDefault();
-	menu.classList.toggle("nav__menu-item-open");
-});
+		evt.preventDefault();
+		menu.classList.toggle("nav__menu-item-open");
+	});
 }
